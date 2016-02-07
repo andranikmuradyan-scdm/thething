068c9d99a899a631ed237e95d390fd13807dfb9a
Andranik <andranik.muradyan@scdm.de>
2016-02-07 10:50:25 +0400

    Added a wrapper for JOptionPane to display message boxes

---
+++ src/thething/core/MessageBox.java
@@ -0,0 +1,12 @@
+package thething.core;
+
+import javax.swing.JOptionPane;
+
+public final class MessageBox {
+  private MessageBox() {
+  }
+  
+  public static void show(String message, String title, int messageType) {
+    JOptionPane.showMessageDialog(null, message, title, messageType);
+  }
+}
